01eed8c4683b87c260aacca4a0e4bfc63de1830e
Johan Martinsson <martinsson.johan@gmail.com>
2012-07-11 22:43:47 +0200

    initialized project

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject agile-grenoble-backend "1.0.0-SNAPSHOT"
+  :description "backend serving session data for Agile Grenoble"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                   [compojure "1.1.0"]])
