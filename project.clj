71ff71cfd0f4c69e230bac98cdc0e7ccb8f464dd
Johan Martinsson <martinsson.johan@gmail.com>
2012-07-11 23:45:49 +0200

    now able to respond with json

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject agile-grenoble-backend "1.0.0-SNAPSHOT"
   :description "backend serving session data for Agile Grenoble"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                   [compojure "1.1.0"]]
+                   [compojure "1.1.0"]
+                   [clj-json "0.5.1"]]
   :plugins [[lein-ring "0.7.1"]]
   :ring {:handler agile-grenoble-backend.core/app})
--- src/agile_grenoble_backend/core.clj
+++ src/agile_grenoble_backend/core.clj
@@ -1,10 +1,17 @@
 (ns agile-grenoble-backend.core  
   (:use compojure.core)
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [clj-json.core :as json]))
+
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
 
 (defroutes main-routes
   (GET "/" [] "<h1>Bonjour Agile Grenoble !</h1>")
+  (GET "/json" [] (json-response {"hello" "world"}))
   (route/resources "/")
   (route/not-found "Page not found"))
 
