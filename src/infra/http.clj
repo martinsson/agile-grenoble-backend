101a72cc9e702de10932b991046f336c5ca1526f
Johan Martinsson <martinsson.johan@gmail.com>
2012-07-17 00:07:30 +0200

    got compojure to work with handlers under routes

--- src/infra/http.clj
+++ src/infra/http.clj
@@ -4,12 +4,18 @@
             [compojure.handler :as handler]
             [core.program-import :as pi]))
 
+(defn what-is-my-ip [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (:remote-addr request)})
 
 (defroutes main-routes
   (GET "/" [] "<h1>Bonjour Agile Grenoble !</h1>")
   (GET "/json" [] (pi/json-response {"hello" "world"}))
+  (GET "/son2" request (what-is-my-ip request))
   (route/resources "/")
   (route/not-found "Page not found"))
 
 (def app
-  (handler/site main-routes))
+  (-> main-routes
+    (handler/site)))
