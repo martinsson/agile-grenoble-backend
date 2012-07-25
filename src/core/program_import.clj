d7c71765a42a9d783fc123489612f9d1e28669e2
Johan Martinsson <martinsson.johan@gmail.com>
2012-07-25 23:52:19 +0200

    cleaning the csv of the empty line at position 1

--- src/core/program_import.clj
+++ src/core/program_import.clj
@@ -8,6 +8,19 @@
 (fact
   (first sessions) => (contains [#"Format.*"]))
 
+(defn empty-csv-line? [line]
+  (every? empty? line))
+
+(fact
+  (empty-csv-line? ["" "" ""]) => true 
+  (empty-csv-line? ["" "a" ""]) => false )
+
+(defn filter-empty-line [csv]
+  (remove empty-csv-line? csv))
+
+(fact 
+  (second sessions) => empty-csv-line?
+  (second (filter-empty-line sessions)) =not=> empty-csv-line?)
 
 (defn get-session-3 [request]
   (let [line (nth sessions 3)
@@ -16,8 +29,12 @@
      :body (zipmap header line) }))
 
 
-(defn append-slot-header [csv]
+(defn append-slot-to-header [csv]
   (cons (cons "Créneau" (first csv)) (rest csv)))
 
-(fact
-  (first (append-slot-header sessions)) => (has-prefix ["Créneau"]))
+(defn append-slot-to-body [csv]
+  (list (first csv) (second csv)))
+(facts
+  (first (append-slot-to-header sessions)) => (has-prefix ["Créneau"])
+  (second (append-slot-to-body (filter-empty-line sessions))) => (has-prefix ["8:30"])
+  )
