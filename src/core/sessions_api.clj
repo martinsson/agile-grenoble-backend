1e3c8194621a18f7ee65b4d648c97ec1f9cd923c
Johan Martinsson <martinsson.johan@gmail.com>
2012-08-26 15:04:27 +0200

    started adding implementation for sessions-for(slot)

--- src/core/sessions_api.clj
+++ src/core/sessions_api.clj
@@ -2,6 +2,8 @@
   (:use midje.sweet)
   (:require [core.adding-missing-data :as amd]))
 
+;TODO rename to butterfly api
+
 (def time-slots ["8:30" "10:00" "11:00"])
 
 (defn slot-list []
@@ -14,12 +16,23 @@
                   3 "11:00"})
 
 (defn sessions-for [slot]
+  (let [sessions (vec (sessions-as-maps amd/cleaned-sessions))
+        session  (sessions (dec slot))
+        key-dictionary {"id" :id
+                        "Titre de la session | Title" :title}
+        normalized-session (merge {:room nil} (clojure.set/rename-keys session key-dictionary))]
+    (select-keys normalized-session [:room :id :title])))
+
+(facts 
+  (sessions-for 2) => {:id 55 
+                       :title "Kanban basics" 
+                       :room nil}
+  (provided (sessions-as-maps amd/cleaned-sessions) => 
+            [{"Créneau | Slot" 3}
+             {"Créneau | Slot" 2 "Titre de la session | Title" "Kanban basics" "id" 55}])
   )
 
-(future-facts 
-  (sessions-for 2) => {:id 3 
-                       :title "Duo de retour d’expérience sauce aigre douce " 
-                       :room nil})
+
 
 (defn sessions-as-maps [parsed-csv]
   (let [header (first parsed-csv)
@@ -32,3 +45,5 @@
   (sessions-as-maps amd/cleaned-sessions) => (has every? #(% "Titre de la session | Title"))
   (first (sessions-as-maps amd/cleaned-sessions)) => (contains {"Titre de la session | Title" "Approche pragmatique pour industrialiser le développement d’applications"})
   (second (sessions-as-maps amd/cleaned-sessions)) => (contains {"Titre de la session | Title" "Challenge Kanban"}))
+
+
