c4c7e84a65e50f6060971dea6edb42b9e9c140c0
Johan Martinsson <martinsson.johan@gmail.com>
2012-09-05 21:48:13 +0200

    implemented a function that will normalize the column headers from the beginning from the beginning

--- src/core/program_import.clj
+++ src/core/program_import.clj
@@ -24,3 +24,29 @@
       (second (sessions)) => empty-csv-line?
       (second (filter-empty-line (sessions))) =not=> empty-csv-line?)
 
+(def key-dictionary {"id" :id
+                     "Titre de la session | Title" :title
+                     "Créneau | Slot" :slot
+                     "Résume | Abstract" :abstract
+                     "Quels bénéfices vont en retirer les participants ? | What will be the benefits for the participants?" :benefits
+                     "Format | Format" :format
+                     "Thèmes | Themes" :theme
+                     "Prénom | First Name" :firstname
+                     "Nom | Last Name" :lastname})
+
+(defn sessions2 []
+  (let [sessions            (sessions)
+        header              (first sessions)
+        body                (rest sessions)
+        replace-if-possible #(get-in key-dictionary [%] %)
+        translated-header   (map replace-if-possible header)]
+    (cons translated-header body)))
+
+    (fact "The csv headers are cleaned"
+          (first (sessions2)) => (contains [:title :abstract :benefits :format :theme :firstname :lastname] :in-any-order :gaps-ok))
+    (fact "The body is unchanged"
+          (rest (sessions2))   => [...line1... 
+                                  ...line2...]
+          (provided (parse-csv anything) => [..header..
+                                             ...line1...
+                                             ...line2...]))
