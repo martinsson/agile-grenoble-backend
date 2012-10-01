5ac65c25cf974df24eb5c0e03dacb1a69a5f246f
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-10-01 22:53:19 +0200

    display program with rooms

--- show_program.js
+++ show_program.js
@@ -1,95 +1,70 @@
-var program = 
-[
+var program = {
+    "rooms" : [
+        "Auditorium",
+        "Makalu",
+        "Kilimandjaro1",
+        "Kilimandjaro3",
+        "Mont blanc 1+2",
+        "Mont blanc 3",
+        "Mont blanc 4",
+        "Cervin",
+        "Evrest"
+    ],
+    "slots": 
     [
-      {"slot":0, 'title':"Accueil des participants autour d'un café", "id":0}  
-    ],
-    [
-      {"slot":0, 'title':"Session Plénière: le mot des organisateurs & Sponsor XYZ", "id":0}  
-    ],
-    [
-      {"slot":0, 'title':"Keynote 1", "id":0}  
-    ],
-    [
-	{"slot":"1","title":"Challenge Kanban","id":"2","speakers": ['John doe', 'Captain Morgan']},
-	{"slot":"1","title":"Duo de retour d’expérience sauce aigre douce ","id":"3"},
-	{"slot":"1","title":"Des mots, des maux ? Démo !","id":"7"},
-	{"slot":"1","title":"Don Quichotte et Sancho testa","id":"8"},
-	{"slot":"1","title":"DevOps@Kelkoo","id":"10"},
-	{"slot":"1","title":"Sky Castle Game","id":"15"},
-	{"slot":"1","title":"Product Owner : comment tirer toute la puissance de l'agilité","id":"16"},
-	{"slot":"1","title":"Retour d’expérience : Agile contre Cycle en V : Le match","id":"17"},
-	{"slot":"1","title":"Il était une fois la vie d’un Product Owner","id":"18"}
-    ],
-    [
-        {"slot":"2","title":"Scrum à Kanban : 3 retours d’expériences","id":"21"},
-        {"slot":"2","title":"Les tests et l'agilité : la vraie vie","id":"24"},
-        {"slot":"2","title":"Changer pour mieux coder","id":"26"},
-        {"slot":"2","title":"Différences entre approches agiles","id":"28"},
-        {"slot":"2","title":"Qualité: Stop à la procrastination","id":"29"},
-        {"slot":"2","title":"La fleur de lotus ou comment redynamiser vos rétrospectives","id":"30"},
-        {"slot":"2","title":"La fleur de rose ou comment redynamiser vos rétrospectives","id":"31"},
-        {"slot":"2","title":"La fleur de yuka ou comment redynamiser vos rétrospectives","id":"32"},
-        {"slot":"2","title":"La fleur de sel ou comment redynamiser vos rétrospectives","id":"33"}
-    ],
-    [
-      {"slot":0, 'title':"Session Plénière: le mot des organisateurs & Sponsor ABC", "id":0}  
-    ],
-    [
-      {"slot":0, 'title':"Keynote 2", "id":0}  
-    ],
-     [
-	{"slot":"1","title":"Challenge Kanban","id":"2"},
-	{"slot":"1","title":"Duo de retour d’expérience sauce aigre douce ","id":"3"},
-	{"slot":"1","title":"Des mots, des maux ? Démo !","id":"7"},
-	{"slot":"1","title":"Don Quichotte et Sancho testa","id":"8"},
-	{"slot":"1","title":"DevOps@Kelkoo","id":"10"},
-	{"slot":"1","title":"Sky Castle Game","id":"15"},
-	{"slot":"1","title":"Product Owner : comment tirer toute la puissance de l'agilité","id":"16"},
-	{"slot":"1","title":"Retour d’expérience : Agile contre Cycle en V : Le match","id":"17"},
-	{"slot":"1","title":"Il était une fois la vie d’un Product Owner","id":"18"}
-    ],
-    [
-        {"slot":"2","title":"Scrum à Kanban : 3 retours d’expériences","id":"21"},
-        {"slot":"2","title":"Les tests et l'agilité : la vraie vie","id":"24"},
-        {"slot":"2","title":"Changer pour mieux coder","id":"26"},
-        {"slot":"2","title":"Différences entre approches agiles","id":"28"},
-        {"slot":"2","title":"Qualité: Stop à la procrastination","id":"29"},
-        {"slot":"2","title":"La fleur de lotus ou comment redynamiser vos rétrospectives","id":"30"},
-        {"slot":"2","title":"La fleur de rose ou comment redynamiser vos rétrospectives","id":"31"},
-        {"slot":"2","title":"La fleur de yuka ou comment redynamiser vos rétrospectives","id":"32"},
-        {"slot":"2","title":"La fleur de sel ou comment redynamiser vos rétrospectives","id":"33"}
-    ],
-    [
-        {"slot":"2","title":"Scrum à Kanban : 3 retours d’expériences","id":"21"},
-        {"slot":"2","title":"Les tests et l'agilité : la vraie vie","id":"24"},
-        {"slot":"2","title":"Changer pour mieux coder","id":"26"},
-        {"slot":"2","title":"Différences entre approches agiles","id":"28"},
-        {"slot":"2","title":"Qualité: Stop à la procrastination","id":"29"},
-        {"slot":"2","title":"La fleur de lotus ou comment redynamiser vos rétrospectives","id":"30"},
-        {"slot":"2","title":"La fleur de rose ou comment redynamiser vos rétrospectives","id":"31"},
-        {"slot":"2","title":"La fleur de yuka ou comment redynamiser vos rétrospectives","id":"32"},
-        {"slot":"2","title":"La fleur de sel ou comment redynamiser vos rétrospectives","id":"33"}
-    ],
-    [
-      {"slot":0, 'title':"Apéro offert par le CARA à tous les présents", "id":0}  
-    ],
-    [
-      {"slot":0, 'title':"Fin de la Journée", "id":0}  
-    ],
-];
-$.each(program, function(islot, slot) {
+        {
+            "all": {"title":"Accueil des participants autour d'un café"}
+        },
+        {
+            "all": {"title":"Session Plénière: le mot des organisateurs & Sogilis"}
+        },
+        {
+            "all": {"title":"Keynote : Reinventing software quality","speakers":["Gojko Adciz"]}
+        },
+        {
+            "Auditorium": {"speakers":["Alain  DELAFOSSE","Nicoals  CAPPONI"," "," "],"slot":"1","title":"DevOps@Kelkoo","id":"10"},
+            "Evrest": {"speakers":["Jean DUPUIS","Christophe  DUPLAIX"," "," "],"slot":"1","title":"Product Owner : comment tirer toute la puissance de l'agilité","id":"16"},
+            "Cervin": {"speakers":["Rémy Sanlaville","Johan Martinsson"," "," "],"slot":"1","title":"Changer pour mieux coder","id":"26"},
+            "Mont blanc 4": {"speakers":["Laurent Tardif","Sandrine Montusclat"," "," "],"slot":"1","title":"Strategie de tests","id":"31"},
+            "Mont blanc 3": {"speakers":["Cyril Megard","Emmanuel Etasse"," "," "],"slot":"1","title":"C'est grave Docteur Rétro ?","id":"39"},
+            "Mont blanc 1+2": {"speakers":["Jean-François Jagodzinski"," "," "," "],"slot":"1","title":"L'agilité pour les nuls","id":"65"},
+            "Kilimandjaro3": {"speakers":["Thomas Ducrocq","Andréane Vuillamy"," "," "],"slot":"1","title":"comment gérer l’absence des Product Owner ?","id":"68"},
+            "Kilimandjaro1": {"speakers":["Pierre Morize"," "," "," "],"slot":"1","title":"Construire une vision partagée","id":"72"},
+            "Makalu": {"speakers":["Christophe NEY"," "," "," "],"slot":"1","title":"Outils informatiques agiles au sein d'une agence digitale","id":"79"}
+        }
+    ]
+};
+    
+ 
+    
+var slot_id = 0;    
+$.each(program["slots"], function(islot, slot) {
     var session_html = '';
-    if (slot.length == 1) {
-        session_html += '<td colspan="9" class="plenary">'+slot[0]['title']+'</td>';
+    if (slot.all) {
+        // there is only one key
+        $.each(slot, function (room, slot) {
+            session_html += '<td colspan="9" class="plenary">'+slot['title']+'</td>';
+        });
+        $('#program_content').append('<tr>'+session_html+'</tr>');
     } else {
-        $.each(slot, function (isession, session) {
+        var room_map = {};
+        var room_nb = 0;
+        $.each(program["rooms"], function (iroom, room) {
+            room_map[room] = room_nb;
+            session_html += '<td id="'+slot_id+'_'+room_nb+'"></td>';
+            room_nb++;
+        });
+        $('#program_content').append('<tr>'+session_html+'</tr>');
+
+        $.each(slot, function (room, session) {
             session_content = '';
             session_content += '<span class="session_title">'+session['title']+'</span> ';
             if (session['speakers']) {
                 session_content += '<span class="session_speakers">'+session['speakers'].join(', ')+'</span>';
             }
-            session_html += '<td>'+session_content+'</td>';
+            session_id = 
+            $('#'+slot_id+'_'+room_map[room]).append(session_content);
         });
     }
-    $('#program_content').append('<tr>'+session_html+'</tr>');
+    slot_id++;
 });
