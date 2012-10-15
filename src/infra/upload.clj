8129b825a057255a82865d4c531be3b8d1b7128a
Johan Martinsson <martinsson.johan@gmail.com>
2012-10-16 01:27:20 +0200

    fixed the origin of local-file

--- src/infra/upload.clj
+++ src/infra/upload.clj
@@ -1,7 +1,7 @@
 (ns infra.upload
   (:use [net.cgrand.enlive-html :only [deftemplate]]
         [clojure.java.io :only (file copy)]
-        [infra.handlers :only (session-maps smaps)]))
+        [infra.handlers :only (session-maps smaps local-file)]))
   
 (defn render [t]
       (apply str t))
