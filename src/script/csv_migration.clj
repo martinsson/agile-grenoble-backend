50ae8473a774c55bdf5c989aec64826e3f056137
Johan Martinsson <martinsson.johan@gmail.com>
2014-09-05 01:07:04 +0200

    migration main with commands

--- src/script/csv_migration.clj
+++ src/script/csv_migration.clj
@@ -5,30 +5,38 @@
 (defn extractor [session-map] (update-in (select-keys session-map ["slides" :width :bio :benefits :length :room :retained :title :speakers
                                                                    :firstname :lastname :theme :abstract "email" :id :slot]) [:speakers] first))
 
-(defn -main [& [url]]
+(defn -main [command & [url]]
   (let [db-spec (or url "postgresql://localhost:5432/sessions")
         csv-file (h/session-maps-file h/local-file)]
-    (jdbc/db-do-commands db-spec (jdbc/drop-table-ddl :sessions))
-    (jdbc/db-do-commands db-spec
-      (jdbc/create-table-ddl :sessions
-      ["slides" "varchar(255)"]
-      [:width "varchar(32)"]
-      [:bio  "text"]
-      [:benefits  "text"]
-      [:length  "varchar(32)"]
-      [:room  "varchar(32)"]
-      [:retained "varchar(32)"]
-      [:title  "varchar(255)"]
-      [:speakers  "varchar(255)"]
-      [:firstname  "varchar(32)"]
-      [:lastname  "varchar(32)"]
-      [:theme  "varchar(32)"]
-      [:abstract  "text"]
-      ["email"  "varchar(32)"]
-      [:id  "varchar(32)"]
-      [:slot  "varchar(32)"]))
-    (for [session-map (map extractor csv-file)] (jdbc/insert! db-spec :sessions session-map))))
+    (case command
+      "drop" (jdbc/db-do-commands db-spec (jdbc/drop-table-ddl :sessions))
+      "create"
+        (jdbc/db-do-commands db-spec
+          (jdbc/create-table-ddl :sessions
+          ["slides" "varchar(255)"]
+          [:width "varchar(32)"]
+          [:bio  "text"]
+          [:benefits  "text"]
+          [:length  "varchar(32)"]
+          [:room  "varchar(32)"]
+          [:retained "varchar(32)"]
+          [:title  "varchar(255)"]
+          [:speakers  "varchar(255)"]
+          [:firstname  "varchar(32)"]
+          [:lastname  "varchar(32)"]
+          [:theme  "varchar(32)"]
+          [:abstract  "text"]
+          ["email"  "varchar(32)"]
+          [:id  "varchar(32)"]
+          [:slot  "varchar(32)"]))
+      "inject" (for [session-map (map extractor csv-file)] (jdbc/insert! db-spec :sessions session-map))
+      "either of [drop, create or inject")))
 
+(defn toto [route]
+  (case route
+    "home" "go home"
+    "away" "go away"
+    "stay"))
 ;(clojure.pprint/pprint line-one)
 
 ;  [:speaker-list "varchar(32)"]
