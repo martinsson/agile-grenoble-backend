27aaa9650bc685e1d0d3a20187d26fa641ecc9aa
Johan Martinsson <martinsson.johan@gmail.com>
2014-09-15 23:16:45 +0200

    get title and co from the propile session

--- src/script/propile_client.clj
+++ src/script/propile_client.clj
@@ -5,7 +5,12 @@
              :body 
              :program_entries 
              (filter :session)))
-(comment depart (:span_entire_row :session :comment :program_id :updated_at :session_id :created_at :track :id :slot))
+(comment depart (:span_entire_row :session :comment :program_id :updated_at :session_id :created_at :track :id :slot)
+         (:intended_audience :first_presenter_id :duration :session_type :materials_needed :topic 
+          :state :laptops_required :updated_at :material_description :first_presenter :title :created_at 
+          :other_limitations :material_url :experience_level :session_goal :short_description 
+          :outline_or_timetable :max_participants :id :description :second_presenter_id :sub_title :room_setup)
+)
 (comment but (:width  :slides :benefits :length :room :retained :title :speakers :email :firstname :type :lastname :theme :abstract :id :slot))
 
 (defn width [session] (if (:span_entire_row session) 1 8))
@@ -16,9 +21,16 @@
    :id (:id session)
    :room (room-name :track)
    :title (get-in session [:session :title])
-   :speakers (remove nil? [(get-in session [:session :first_presenter :name]) (get-in session [:session :second_presenter :name])])})
+   :sub_title (get-in session [:session :sub_title]) ;??
+   :abstract (get-in session [:session :short_description])
+   :description (get-in session [:session :description])
+   :benefits (get-in session [:session :session_goal]) ;??
+   
+   :speakers (remove nil? [(get-in session [:session :first_presenter :name]) (get-in session [:session :second_presenter :name])])
+   :type (get-in session [:session :session_type ])
+   })
 
-(clojure.pprint/pprint (backend-session (nth result 5)))
+(clojure.pprint/pprint (backend-session (nth result 3)))
 
 
 ;todo speaker.name
