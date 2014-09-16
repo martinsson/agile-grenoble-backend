9fd91445b586fbe6576f12ff94382205e85edad5
Johan Martinsson <martinsson.johan@gmail.com>
2014-09-16 21:38:05 +0200

    added slot, theme and length to propile version of program

--- src/script/propile_client.clj
+++ src/script/propile_client.clj
@@ -1,5 +1,5 @@
 (ns script.propile-client)
-(defonce propile-response (client/get "http://cfp.agile-grenoble.org/programs/5/full_export" {:as :json}))
+(defonce propile-response (client/get "http://cfp.agile-grenoble.org/programs/6/full_export" {:as :json}))
 
 (def result (->> propile-response 
              :body 
@@ -11,7 +11,7 @@
           :other_limitations :material_url :experience_level :session_goal :short_description 
           :outline_or_timetable :max_participants :id :description :second_presenter_id :sub_title :room_setup)
 )
-(comment but (:width  :slides :benefits :length :room :retained :title :speakers :email :firstname :type :lastname :theme :abstract :id :slot))
+(comment but (  :slides  :length :room  :email :firstname  :lastname))
 
 (defn width [session] (if (:span_entire_row session) 1 8))
 
@@ -25,9 +25,11 @@
    :abstract (get-in session [:session :short_description])
    :description (get-in session [:session :description])
    :benefits (get-in session [:session :session_goal]) ;??
-   
+   :theme (get-in session [:session :topic])
    :speakers (remove nil? [(get-in session [:session :first_presenter :name]) (get-in session [:session :second_presenter :name])])
    :type (get-in session [:session :session_type ])
+   :slot (:slot session)
+   :length 3
    })
 
 (clojure.pprint/pprint (backend-session (nth result 3)))
