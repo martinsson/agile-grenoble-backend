67ad469cf656506a538fa19088059fcb9b612e69
Johan Martinsson <martinsson.johan@gmail.com>
2015-10-16 00:29:39 +0200

    ajout dapero + suppression de code mort encore

--- src/core/sessions_api.clj
+++ src/core/sessions_api.clj
@@ -1,17 +1,11 @@
 (ns core.sessions-api
   (:use midje.sweet))
 
-(def time-slots ["10:00" "10:15" "10:30" "10:45" "11:05" "11:20" "11:35" "11:50" "13:20" 
-                 "13:45" "14:30"  "14:50" "15:05" "15:20" "15:35" "16:05" "16:20" "16:35" 
-                 "16:50" "17:10" "17:25" "17:40" "17:55" "18:15"])
-(def slot-list-basic (zipmap (iterate inc 1) time-slots))
-
 (defn session-list-for 
   ([session-maps slot]
   (let [reduce-keys #(select-keys % [:id :title :slot :room :speakers :type :length :theme :width])
         found-sessions (filter #(= slot (% :slot)) session-maps)] 
-    (for [s (map reduce-keys found-sessions)]
-              (assoc-in s [:start-time] (slot-list-basic (Integer/valueOf (:slot s)))) ))))
+    (map reduce-keys found-sessions))))
 
   (facts "find the sessions for a given slot"
     (session-list-for [{:slot 3} 
--- src/infra/handlers.clj
+++ src/infra/handlers.clj
@@ -17,7 +17,7 @@
                "Mt-Blanc 3" {:id 8, :capacity 24}
                "Mt-Blanc 4" {:id 9, :capacity 24}
                "Atrium"     {:id 10 :capacity 100}})
-
+(def room-count (count room-defs))
 
 (defn add-non-session-data [[s1 s2 s3 s4 s5 s6 s7 s8]]
   (let [non-sessions  [{:title "Accueil des participants autour d'un café" :type :non-session}
@@ -30,19 +30,21 @@
                        {:title "Pause café" :type :cafe}
                        {:title "Pause café" :type :cafe}
                        {:title "Changement de salle" :type :non-session}
+                       {:title "Changement de salle" :type :non-session}
+                       {:title "Apéro offert par le CARA" :type :non-session}
                        
                        ;{:title "Fin de journée" :type :departure}
                        ]
-        [arr sp1 chgmt1 cafe1 meal sp2 chgmt2 cafe2 cafe3 chgmt3 chgmt4] (for [ns non-sessions] {"all" (assoc ns :length 1 :width 11)})]
-    [arr sp1 s1 chgmt1 s2 cafe1 s3 meal sp2 s4 chgmt2 s5 cafe2 s6 cafe3 s7 chgmt3 s8]))
+        [arr sp1 chgmt1 cafe1 meal sp2 chgmt2 cafe2 cafe3 chgmt3 chgmt4 apero] (for [ns non-sessions] {"all" (assoc ns :length 1 :width room-count)})]
+    [arr sp1 s1 chgmt1 s2 cafe1 s3 meal sp2 s4 chgmt2 s5 cafe2 s6 cafe3 s7 chgmt3 s8 apero]))
 
 
 
 (defn all-slots-with-rooms 
   
-  ([smaps] (let [;header   (keys (first @smaps))
+  ([smaps] (let [more-than-number-of-slots 15  ;header   (keys (first @smaps))
           index-by-room #(zipmap (map :room %) %)
-          slots    (for [slot (range 1 15)] (sa/session-list-for smaps slot))
+          slots    (for [slot (range 1 more-than-number-of-slots)] (sa/session-list-for smaps slot))
           all-slots (add-non-session-data (map index-by-room slots))]
       {:rooms room-defs
        :slots (remove empty? all-slots)
